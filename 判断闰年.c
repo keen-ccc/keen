0624113f041128143d56bd0794ca1ac52c870be5
keen-ccc <72068128+keen-ccc@users.noreply.github.com>
2020-12-30 21:36:59 +0800

    Create 判断闰年.c

---
+++ 判断闰年.c
@@ -0,0 +1,19 @@
+//判断闰年
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n%100==0){
+		if(n%400==0)
+		printf("1");
+		else
+		printf("0");
+	}
+	else{
+		if(n%4==0&&n%100!=0)
+		printf("1");
+		else
+		printf("0");
+	}
+ } 
