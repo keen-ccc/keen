81d61ff3e32fe28c5389b136b797869e9fa2c887
keen-ccc <72068128+keen-ccc@users.noreply.github.com>
2021-01-03 21:43:31 +0800

    Create 最小的数.c

---
+++ 最小的数.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int i=0,a[11]={0};
+	
+	for(i=0;i<10;i++){
+		scanf("%d",&a[i]);
+	}
+	for(i=1;i<10;i++){
+		if(a[i]!=0){
+			printf("%d",i);
+			a[i]--;
+			break;
+		}
+	}
+	for(i=0;i<10;i++){
+		while(a[i]!=0){
+			printf("%d",i);
+			a[i]--;
+		}
+	}
+	printf("\n");
+	
+}
