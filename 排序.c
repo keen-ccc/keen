9b94fa3d4e7d9c2aa76dfd97dd9ed3de3a2c6b07
keen-ccc <72068128+keen-ccc@users.noreply.github.com>
2021-01-10 11:08:00 +0800

    Create 排序.c

---
+++ 排序.c
@@ -0,0 +1,23 @@
+//排序
+
+
+for(i=0;i<p;i++){
+	for(j=0;j<p-1-i;j++){
+		if(a[j]>a[j+1]){
+			t=a[j];
+			a[j]=a[j+1];
+			a[j+1]=t;
+		}
+	}
+} 
+
+
+for(i=0;i<p;i++){
+	for(j=0;j<i;j++){
+		if(a[j]>a[i]){
+			t=a[j];
+			a[j]=a[k];
+			a[k]=t;
+		}
+	}
+}
