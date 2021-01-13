27a4bab47865aae1bfe2787e577ca260246c1906
keen-ccc <72068128+keen-ccc@users.noreply.github.com>
2021-01-13 11:20:10 +0800

    Update 排序.c

--- 排序.c
+++ 排序.c
@@ -11,6 +11,16 @@ for(i=0;i<p;i++){
 	}
 } 
 
+for(i=0;i<p-1;i++){
+	for(j=0;j<p-1-i;j++){
+	if(a[j]>a[j+1]{
+		t=a[j];
+		a[j]=a[j+1];
+		a[j+1]=t;
+	}
+	}
+
+}
 
 for(i=0;i<p;i++){
 	for(j=0;j<i;j++){
